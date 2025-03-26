3eea77e80247bdfcd46625bba7d3af78e85879b8
hattorisaki <nicebackfisch@gmail.com>
2025-03-26 11:10:03 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,12 @@ function toggleMenu() {
 function scrollGallery(direction) {
     const gallery = document.querySelector('.gallery');
     const scrollAmount = 300; // Wieviel Pixel gescrollt wird
-    const images = document.querySelectorAll('.gallery img');
-        
-    images.forEach(img => {
-    let clone = img.cloneNode(true);
-    galleryContent.appendChild(clone);
-    });    
+    
 
     gallery.scrollBy({
         left: direction * scrollAmount,
         behavior: 'smooth'
     });
 
-    gallery.style.transform = `translateX(${-scrollAmount}px)`;    
+     
 }
